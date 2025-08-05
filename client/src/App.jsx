a5ca872b4c515c8e4605bb6cbe24b188352c7b81
SaiGanesh <saiganeshambati000@gmail.com>
2025-08-05 08:08:16 +0530

    hackoverflow page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
+import Hackoverflow from './User/Events/HackoverFlow/Hackoverflow';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Footer />
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Navbar />} /> */}
+          <Route path="/" element={<Hackoverflow />} />
+        </Routes>
+        <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
