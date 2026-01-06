33e1197f66345a22c43a1241d892b1c01095d127
codevoid048 <williamkeri007@gmail.com>
2026-01-06 13:54:31 +0530

    refactor: replace react-helmet-async with @dr.pogodin/react-helmet for SEO management.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from '@dr.pogodin/react-helmet';
 import './index.css'
 import App from './App.jsx'
 
